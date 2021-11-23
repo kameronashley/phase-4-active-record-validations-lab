cc15406fe7cb57216d92e727743384b54cace3c0
Kameron Ashley <kameronashley@gmail.com>
2021-11-23 15:03:44 -0800

    commit tree 61eb890768e3cc75440df10021274d99c5155855

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  
+  validate :click_bait
+
+  def click_bait
+    if title
+        unless ["Won’t Believe", "Secret", "Top [number]", "Guess"].any?{|phrase| title.include? phrase}
+          errors.add(:title, "This is not enough clickbait")
+        end
+     end
+  end
+
+
+
 end
+
+
+# parameters = ["Won't Believe","Secret","Top #{5..10}","Guess"]
